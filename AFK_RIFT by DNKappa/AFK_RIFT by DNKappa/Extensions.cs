99a281e052892a7a664a4196ec1f972a3ee2aaa5
scarEl <inspirationgamer@yahoo.de>
2016-12-30 20:42:09 +0100

    AFK_Rift

---
+++ AFK_RIFT by DNKappa/AFK_RIFT by DNKappa/Extensions.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+
+using EloBuddy;
+using SharpDX;
+
+namespace AFK_RIFT_by_DNKappa
+{
+    internal static class Extensions
+    {
+        static Extensions()
+        {
+
+        }
+
+        internal static float Distance(this Obj_AI_Base unit, Vector3 point, bool squared = false)
+        {
+            return unit.ServerPosition.To2D().Distance(point.To2D(), squared);
+        }
+
+        internal static float Distance(this Vector2 v, Vector2 to, bool squared = false)
+        {
+            return squared ? Vector2.DistanceSquared(v, to) : Vector2.Distance(v, to);
+        }
+
+        internal static Vector2 To2D(this Vector3 v)
+        {
+            return new Vector2(v.X, v.Y);
+        }
+
+        internal static bool HasItem(int id, Obj_AI_Base hero = null)
+        {
+            return (hero ?? ObjectManager.Player).InventoryItems.Any(slot => slot.Id == (ItemId)id);
+        }
+
+        internal static bool UseItem(int id, Vector3 position)
+        {
+            if (position != Vector3.Zero)
+            {
+                foreach (var slot in ObjectManager.Player.InventoryItems.Where(slot => slot.Id == (ItemId)id))
+                {
+                    return ObjectManager.Player.Spellbook.CastSpell(slot.SpellSlot, position);
+                }
+            }
+
+            return false;
+        }
+
+        internal static float GetRealAutoAttackRange(AttackableUnit target)
+        {
+            var result = ObjectManager.Player.AttackRange + ObjectManager.Player.BoundingRadius;
+            if (target != null && target.IsValid)
+            {
+                var aiBase = target as Obj_AI_Base;
+                if (aiBase != null && ObjectManager.Player.ChampionName == "Caitlyn")
+                {
+                    if (aiBase.HasBuff("caitlynyordletrapinternal"))
+                    {
+                        result += 650;
+                    }
+                }
+
+                return result + target.BoundingRadius;
+            }
+
+            return result;
+        }
+
+        internal static bool InAutoAttackRange(AttackableUnit target)
+        {
+            if (target == null || target.IsDead)
+            {
+                return false;
+            }
+
+            var myRange = GetRealAutoAttackRange(target);
+
+            return Vector2.DistanceSquared(
+                    target is Obj_AI_Base ? ((Obj_AI_Base)target).ServerPosition.To2D() : target.Position.To2D(),
+                    ObjectManager.Player.ServerPosition.To2D()) <= myRange * myRange;
+        }
+
+        public static bool InShop(this Obj_AI_Base unit)
+        {
+            float fountainRange = 562500; //750 * 750
+
+            if (Game.MapId == GameMapId.SummonersRift)
+            {
+                fountainRange = 1000000; //1000 * 1000
+            }
+
+            var fpos = unit.Team == ObjectManager.Player.Team ?
+                ObjectManager.Get<Obj_Shop>().FirstOrDefault(x => x.IsAlly).Position : ObjectManager.Get<Obj_Shop>().FirstOrDefault(x => x.IsEnemy).Position;
+
+            return unit.IsVisible && unit.Distance(fpos, true) <= fountainRange;
+        }
+    }
+}
---
+++ AFK_RIFT by DNKappa/AFK_RIFT by DNKappa/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using SharpDX;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+
+namespace AFK_RIFT_by_DNKappa
+{
+    internal static class Program
+    {
+        private static int _LastOnUpdateTick;
+
+        private static Vector3 PlayerPosition
+        {
+            get
+            {
+                switch (Game.MapId)
+                {
+                    //case GameMapId.CrystalScar:
+                    //    break;
+                    //case GameMapId.TwistedTreeline:
+                    //    break;
+                    case GameMapId.SummonersRift:
+                        switch (ObjectManager.Player.Team)
+                        {
+                            //case GameObjectTeam.Unknown:
+                            //    break;
+                            case GameObjectTeam.Order:
+                                return new Vector3(8572, 3258, 54.43658f);
+                            case GameObjectTeam.Chaos:
+                                return new Vector3(6424, 11656, 54.83006f);
+                            //case GameObjectTeam.Neutral:
+                            //    break;
+                            //default:
+                            //    break;
+                        }
+                        break;
+                    //case GameMapId.HowlingAbyss:
+                    //    break;
+                    //default:
+                    //    break;
+                }
+
+                return Vector3.Zero;
+            }
+        }
+
+        private static Vector3 WardPosition
+        {
+            get
+            {
+                switch (Game.MapId)
+                {
+                    //case GameMapId.CrystalScar:
+                    //    break;
+                    //case GameMapId.TwistedTreeline:
+                    //    break;
+                    case GameMapId.SummonersRift:
+                        switch (ObjectManager.Player.Team)
+                        {
+                            //case GameObjectTeam.Unknown:
+                            //    break;
+                            case GameObjectTeam.Order:
+                                return new Vector3(8476, 2908, 51.13f);
+                            case GameObjectTeam.Chaos:
+                                return new Vector3(6412, 11998, 56.4768f);
+                            //case GameObjectTeam.Neutral:
+                            //    break;
+                            //default:
+                            //    break;
+                        }
+                        break;
+                    //case GameMapId.HowlingAbyss:
+                    //    break;
+                    //default:
+                    //    break;
+                }
+
+                return Vector3.Zero;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Hacks.AntiAFK = true;
+            //Hacks.GameObjectCache = true;
+
+            if (Game.Mode == GameMode.Running)
+            {
+                Game_OnStart(new EventArgs());
+            }
+            else
+            {
+                Game.OnLoad += Game_OnStart;
+            }
+        }
+
+        private static void Game_OnStart(EventArgs args)
+        {
+            if (Game.MapId != GameMapId.SummonersRift)
+            {
+                Chat.Print(
+                    "<font color = \"#00D8FF\"><b>AFK Bot for summoners rift:</b></font> You have to be in Summoner's rift to use this assembly.");
+                return;
+            }
+
+            if (!ObjectManager.Player.IsRanged)
+            {
+                Chat.Print(
+                    "<font color = \"#00D8FF\"><b>AFK Bot for summoners rift:</b></font> You have to be a ranged champion to use this assembly.");
+                return;
+            }
+
+            Game.OnEnd += Game_OnEnd;
+            Game.OnUpdate += Game_OnUpdate;
+
+            Chat.Print("<font color = \"#00D8FF\"><b>AFK Bot for summoners rift:</b></font> Loaded.");
+            Chat.Print("<font color = \"#00D8FF\"><b>All Credits to:</b></font> Definitely not Kappa.");
+        }
+
+        private static void Game_OnEnd(GameEndEventArgs args)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(5000);
+                Game.QuitGame();
+            });
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            var nowTick = Environment.TickCount;
+            if (nowTick - _LastOnUpdateTick < 333)
+            {
+                return;
+            }
+
+            _LastOnUpdateTick = Environment.TickCount;
+
+            if (ObjectManager.Player.IsDead)
+            {
+                return;
+            }
+
+            if (PlayerPosition == Vector3.Zero ||
+                WardPosition == Vector3.Zero)
+            {
+                return;
+            }
+
+            if (ObjectManager.Player.IsInShopRange())
+            {
+                //ObjectManager.Player.BuyItem(ItemId.Vision_Ward);
+                Shop.BuyItem(ItemId.Control_Ward);
+            }
+
+            if (ObjectManager.Player.Distance(PlayerPosition) > 50)
+            {
+                if (ObjectManager.Player.CanMove)
+                {
+                    //ObjectManager.Player.IssueOrder(GameObjectOrder.MoveTo, PlayerPosition);
+                    Player.IssueOrder(GameObjectOrder.MoveTo, PlayerPosition);
+                }
+            }
+            else
+            {
+                var target = ObjectManager.Get<Obj_AI_Base>()
+                            .FirstOrDefault(x => !x.IsDead && !x.IsAlly && Extensions.InAutoAttackRange(x));
+                //if (FogOfWar.InFog(WardPosition))
+                if (Player.Instance.HasItem(ItemId.Control_Ward))
+                {
+                    Extensions.UseItem((int) ItemId.Control_Ward, WardPosition);
+                }
+                if (target != null)
+                {
+
+                        if (ObjectManager.Player.CanAttack)
+                        {
+                            Player.IssueOrder(GameObjectOrder.AttackUnit, target,true);
+                        }
+                    }
+                }
+            }
+        }
+    }
