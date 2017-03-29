512bf42a00c0355c7f887679791eff35dd6888a1
scarEl <inspirationgamer@yahoo.de>
2017-03-29 17:11:04 +0200

    failed.

---
+++ LeguesharpPrediction/LeguesharpPrediction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using Color = System.Drawing;
+
+namespace LeaguesharpPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Onload;
+        }
+
+        private static void Onload(EventArgs args)
+        {
+            Chat.Print(">> Leguesharp Prediction loaded. kek.", Color.Color.BlueViolet);
+        }
+    }
+}
